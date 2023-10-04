abee014708f33595611e63c75f49157e41bea52d
B6K10 <pouhebatoumhansmichel@gmail.com>
2023-10-04 21:03:12 +0100

    commit7

--- tp3Project/src/main/kotlin/example/myapp/decor/Decoration.kt
+++ tp3Project/src/main/kotlin/example/myapp/decor/Decoration.kt
@@ -32,8 +32,22 @@ fun makeDecorations2() {
     println(wood)
     println(diver)
 }
+enum class Color(val rgb: Int) {
+    RED(0xFF0000), GREEN(0x00FF00), BLUE(0x0000FF);
+}
+
+enum class Direction(val degrees: Int) {
+    NORTH(0), SOUTH(180), EAST(90), WEST(270)
+}
 
 fun main(){
     makeDecorations()
     makeDecorations2()
+
+    println(Color.GREEN.ordinal)
+
+    println(Direction.EAST.name)
+    println(Direction.EAST.ordinal)
+    println(Direction.EAST.degrees)
 }
+
