bfc155adc056a812ffdb06b2819010e9002c9711
B6K10 <pouhebatoumhansmichel@gmail.com>
2023-10-04 21:49:12 +0100

    commit8

--- tp3Project/src/main/kotlin/example/myapp/decor/Decoration.kt
+++ tp3Project/src/main/kotlin/example/myapp/decor/Decoration.kt
@@ -40,6 +40,13 @@ enum class Direction(val degrees: Int) {
     NORTH(0), SOUTH(180), EAST(90), WEST(270)
 }
 
+class Choice {
+    companion object {
+        var name: String = "lyric"
+        fun showDescription(name:String) = println("My favorite $name")
+    }
+}
+
 fun main(){
     makeDecorations()
     makeDecorations2()
@@ -49,5 +56,9 @@ fun main(){
     println(Direction.EAST.name)
     println(Direction.EAST.ordinal)
     println(Direction.EAST.degrees)
+
+    println(Choice.name)
+    Choice.showDescription("pick")
+    Choice.showDescription("selection")
 }
 
